e8e7f32449668f288c6857aed2fa3b27b1fcc843
Philip Carlsson <philipcarlsson@hotmail.com>
2022-02-04 16:25:13 +0100

    adding the rendering function

--- treasure.js
+++ treasure.js
@@ -9,4 +9,23 @@ class TreasureButton extends React.Component {
         //declaring that to begin with, "treasure" is not found.
         this.state = {opened: false}
     }
+
+    //what happens
+    render() {
+        //if button is pressed
+        if (this.state.opened === true) {
+            return "Hmm.. You see that someone's already been here. Shame."
+        }
+
+        return React.createElement(
+            "div",    //what element to create
+            null,   // no function
+            React.CreateElement(   //Create new element inside div
+                "button",
+                {onClick: () => this.setState({ opened: true })},    //by pressing the button state changes to true
+                "Press the button for a secret Treasure.."
+            )
+        )
+    }
 }
+
