c179843bddb54276ce0d552cd80a0454c9f5b669
Philip Carlsson <philipcarlsson@hotmail.com>
2022-02-04 16:13:03 +0100

    adding TreasureButton class from React.Component

---
+++ treasure.js
@@ -0,0 +1,12 @@
+//Strict mode
+'use strict'
+
+//using react class to create new function
+class TreasureButton extends React.Component {
+    constructor(props) {
+        super(props)
+
+        //declaring that to begin with, "treasure" is not found.
+        this.state = {opened: false}
+    }
+}
