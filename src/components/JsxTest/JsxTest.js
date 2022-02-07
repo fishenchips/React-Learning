75852eba28d7ebc5dd507486bea5953665c8cc8c
Philip Carlsson <philipcarlsson@hotmail.com>
2022-02-07 11:55:38 +0100

    first React from scratch

---
+++ src/components/JsxTest/JsxTest.js
@@ -0,0 +1,12 @@
+// import React 
+import React from "react"
+
+//Function need to begin with Capital letter and same name as the JS file
+let JsxTest = () => {
+    return(
+        <h3 className="h3-Jsx">Test from JsxTest</h3>
+    )
+}
+
+//send to App.js
+export default JsxTest;
